e76267aaf84f25e817eee153bcb94e1c822c325a
Bakkar7 <siddiqabubakar95@gmail.com>
2020-10-20 14:02:53 +0400

    init

---
+++ src/components/PostForm.js
@@ -0,0 +1,81 @@
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import React from "react";
+import { Form, Button } from "semantic-ui-react";
+
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+import { useForm } from "../util/hooks";
+
+export default function PostForm() {
+  const { values, onChange, onSubmit } = useForm(createPostCallback, {
+    body: "",
+  });
+
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    // we will access the cache using proxy amd FETCH_POSTS_QUERY
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      data.getPosts = [result.data.createPost, ...data.getPosts];
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      values.body = "";
+    },
+  });
+
+  function createPostCallback() {
+    createPost();
+  }
+
+  return (
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>create a Post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder="Social Network"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Post
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      createdAt
+      username
+      likes {
+        id
+        username
+        createdAt
+      }
+      likeCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+      commentCount
+    }
+  }
+`;
